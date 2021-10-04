cf0f9895284711d7380bdadbc8e8a124e6a26fc2
notkanishk <singhkanishka1@gmail.com>
2021-10-04 20:51:23 +0530

    Added more birthday messages

--- utils/birthdayWishes.js
+++ utils/birthdayWishes.js
@@ -12,6 +12,21 @@
     {"value":"Work hard. Play hard. Eat lots of cake. That’s a good motto for your birthday and for life."},
     {"value":"Hope you get birthday presents in the dozens!"},
     {"value":"May you have all the love your heart can hold, all the happiness a day can bring, and all the blessings a life can unfold."},
+    {"value":"Be happy! Today is the day you were brought into this world to be a blessing and inspiration to the people around you! You are a wonderful person! May you be given more birthdays to fulfill all of your dreams!"},
+    {"value":"Forget about the past, you can’t change it. Forget about the future, you can’t predict it. And forget about the present, I didn’t get you one. Happy birthday!"},
+    {"value":"As you get older three things happen. The first is your memory goes, and I can’t remember the other two. Happy birthday!"},
+    {"value":"When the little kids ask how old you are at your party, you should go ahead and tell them. While they’re distracted trying to count that high, you can steal a bite of their cake! Happy birthday!"},
+    {"value":"Sending you smiles for every moment of your special day…Have a wonderful time and a very happy birthday!"},
+    {"value":"Sending you a birthday wish wrapped with all my love. Have a very happy birthday!"},
+    {"value":"A simple celebration, a gathering of friends; here wishing you great happiness and a joy that never ends. Happy birthday!"},
+    {"value":"Hope your birthday is just like you…totally freaking awesome."},
+    {"value":"I hope that today, at your party, you dance and others sing as you celebrate with joy your best birthday."},
+    {"value":"It’s always a treat to wish happy birthday to someone so sweet."},
+    {"value":"Hey you! It’s your birthday! Cake! Candles! Drinks! Presents! More food! Can you tell I’m excited?"},
+    {"value":"Happy birthday to my forever young friend!"},
+    {"value":"Birthdays only come once a year, and your friendship only comes once a lifetime."},
+    {"value":"You deserve all the cake, happiness, and love today. Happy birthday!"},
+    {"value":"Thank you for being the brightest person I know. Hope to see you shine for years on! Happy birthday!"},
 
 ]
 
