78db979eeab55a4b1b8dabc2ffed16228f6159c3
PhattOZ <phatto2543@gmail.com>
2021-10-04 12:45:37 +0700

    Allow link copying. Style change when click.

--- components/CopyLinkButton.js
+++ components/CopyLinkButton.js
@@ -1,7 +1,25 @@
-import style from "../styles/CopyLinkButton.module.css"
+import { useState } from "react";
+import style from "../styles/CopyLinkButton.module.css";
 
 const CopyLinkButton = () => {
-    return <button className={style.button}> Copy Link</button>
-}
+  const [active, setActive] = useState(false);
 
-export default CopyLinkButton
+  const handleClick = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setActive(true);
+    setTimeout(() => {
+      setActive(false);
+    }, 3000);
+  };
+
+  return (
+    <button
+      className={style.button + ` ${active ? style.active : ""}`}
+      onClick={handleClick}
+    >
+      {active ? "Link copied!" : "Copy Link"}
+    </button>
+  );
+};
+
+export default CopyLinkButton;
